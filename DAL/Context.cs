7e68cf7057ce2b1260bf830a66babe8f5f9eb88b
Esthiber <esthiber1@gmail.com>
2025-03-09 16:39:12 -0400

    Creacion de clase contexto.

---
+++ DAL/Context.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Esthiber_Valentin_P2_AP1.DAL
+{
+    public class Context(DbContextOptions<Context> options) : DbContext(options)
+    {
+
+    }
+}
