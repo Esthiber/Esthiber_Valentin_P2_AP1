dc366707ab36bff090aea7c896b3b24dfeb40aa6
Esthiber <esthiber1@gmail.com>
2025-03-10 19:19:28 -0400

    Updated the service

--- Services/EncuestasService.cs
+++ Services/EncuestasService.cs
@@ -9,30 +9,46 @@ namespace Esthiber_Valentin_P2_AP1.Services
     {
         public async Task<bool> Insertar(Encuestas encuesta)
         {
-            return false;
+            await using var context = await Dbfactory.CreateDbContextAsync();
+            context.Encuestas.Add(encuesta);
+            return await context.SaveChangesAsync() > 0;
         }
 
         public async Task<bool> Modificar(Encuestas encuesta)
         {
-            return false;
+            await using var context = await Dbfactory.CreateDbContextAsync();
+            context.Encuestas.Update(encuesta);
+            return await context.SaveChangesAsync() > 0;
         }
 
         public async Task<bool> Guardar(Encuestas encuesta)
         {
-            return false;
+            if (!await Existe(encuesta.EncuestaId))
+            {
+                return await Insertar(encuesta);
+            }
+            else
+            {
+                return await Modificar(encuesta);
+            }
         }
 
         public async Task<bool> Eliminar(int id)
         {
-            return false;
+            await using var context = await Dbfactory.CreateDbContextAsync();
+            return await context.Encuestas
+                .Where(e => e.EncuestaId == id)
+                .ExecuteDeleteAsync() > 0;
         }
-        public async Task<bool> Buscar(int id)
+        public async Task<bool> Existe(int id)
         {
-            return false;
+            await using var context = await Dbfactory.CreateDbContextAsync();
+            return await context.Encuestas.AnyAsync(e => e.EncuestaId == id);
         }
 
         public async Task<List<Encuestas>> Listar(Expression<Func<Encuestas, bool>> criterio)
         {
+            await using var context = await Dbfactory.CreateDbContextAsync();
             return new();
         }
     }
