738a41912ea681b53cbe50c95026f079c2293a90
Esthiber <esthiber1@gmail.com>
2025-03-10 18:59:36 -0400

    Add Class Ciudades Service

---
+++ Services/CiudadesService.cs
@@ -0,0 +1,30 @@
+﻿using Esthiber_Valentin_P2_AP1.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace Esthiber_Valentin_P2_AP1.Services
+{
+    public class CiudadesService(IDbContextFactory<Context> Dbfactory)
+    {
+        public enum TipoOperacion
+        {
+            Sumar,
+            Restar
+        }
+
+        public async Task<bool> UpdateMonto(int id, double monto, TipoOperacion tipoOperacion)
+        {
+            await using var context = await Dbfactory.CreateDbContextAsync();
+            if (tipoOperacion == TipoOperacion.Sumar)
+            {
+
+            }
+
+            if (tipoOperacion == TipoOperacion.Restar)
+            {
+
+            }
+
+            return false;
+        }
+    }
+}
