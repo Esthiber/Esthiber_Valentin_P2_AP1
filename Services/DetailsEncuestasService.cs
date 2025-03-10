329beffc37cc7ee864a6ca4e8d2dac149929e411
Esthiber <esthiber1@gmail.com>
2025-03-10 19:29:54 -0400

    Updated methods

--- Services/DetailsEncuestasService.cs
+++ Services/DetailsEncuestasService.cs
@@ -7,28 +7,36 @@ namespace Esthiber_Valentin_P2_AP1.Services
 {
     public class DetailsEncuestasService(IDbContextFactory<Context> Dbfactory)
     {
-        public async Task<bool> Insertar(DetailsEncuestasCiudades model)
+        public async Task<bool> Insertar(DetailsEncuestasCiudades details)
+        {
+            await using var context = await Dbfactory.CreateDbContextAsync();
+            context.DetailsEncuestas.Add(details);
+            return await context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Modificar(DetailsEncuestasCiudades details)
+        {
+            await using var context = await Dbfactory.CreateDbContextAsync();
+            context.DetailsEncuestas.Update(details);
+            return await context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Guardar(DetailsEncuestasCiudades details)
         {
             return false;
         }
 
-        public async Task<bool> Modificar(DetailsEncuestasCiudades model)
-        {
-            return false;
-        }
-
-        public async Task<bool> Guardar(DetailsEncuestasCiudades model)
-        {
-            return false;
-        }
         public async Task<bool> Eliminar(int id)
         {
-            return false;
+            await using var context = await Dbfactory.CreateDbContextAsync();
+            return await context.DetailsEncuestas
+                .Where(d => d.DetailsEncuestasCiudadesId == id)
+                .ExecuteDeleteAsync() > 0;
         }
-
 
         public async Task<List<DetailsEncuestasCiudades>> Listar(Expression<Func<DetailsEncuestasCiudades, bool>> criterio)
         {
+            await using var context = await Dbfactory.CreateDbContextAsync();
             return new();
         }
     }
