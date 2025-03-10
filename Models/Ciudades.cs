254b405249e02e3db76fe7e9b2dd90436df81b46
Esthiber <esthiber1@gmail.com>
2025-03-10 18:39:36 -0400

    Add Class Ciudades

---
+++ Models/Ciudades.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Esthiber_Valentin_P2_AP1.Models
+{
+    public class Ciudades
+    {
+        [Key]
+        public int CiudadesId { get; set; }
+        public string Nombre { get; set; }
+        public double Monto { get; set; }
+    }
+}
