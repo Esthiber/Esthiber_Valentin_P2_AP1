ddb7f2318ff8168faf47950ee66d4f488a71594c
Esthiber <esthiber1@gmail.com>
2025-03-10 19:42:30 -0400

    Inject Services On main

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Esthiber_Valentin_P2_AP1.Components;
 using Esthiber_Valentin_P2_AP1.DAL;
+using Esthiber_Valentin_P2_AP1.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,10 @@ builder.Services.AddRazorComponents()
 
 var ConStr = builder.Configuration.GetConnectionString("ConStr");
 builder.Services.AddDbContextFactory<Context>(o => o.UseSqlServer(ConStr));
+
+builder.Services.AddScoped<EncuestasService>();
+builder.Services.AddScoped<CiudadesService>();
+builder.Services.AddScoped<DetailsEncuestasService>();
 
 var app = builder.Build();
 
