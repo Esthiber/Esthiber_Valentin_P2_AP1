10ddc2e51c160b4b70b3fe33db9cfccb41c809f8
Esthiber <esthiber1@gmail.com>
2025-03-10 18:48:07 -0400

    Add class Details Encuestas-Ciudades

---
+++ Models/DetailsEncuestasCiudades.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Esthiber_Valentin_P2_AP1.Models
+{
+    public class DetailsEncuestasCiudades
+    {
+        [Key]
+        public int DetailsEncuestasCiudadesId { get; set; }
+
+        public int EncuestaId { get; set; }
+
+        public int CuidadId { get; set; }
+
+        public virtual Encuestas Encuesta { get; set; } = null!;
+
+        public virtual Ciudades Ciudad { get; set; } = null!;
+    }
+}
