925a587075d485f063877ea6c201385b934cfbc2
Esthiber <esthiber1@gmail.com>
2025-03-10 23:52:21 -0400

    Updating property name

--- Models/DetailsEncuestasCiudades.cs
+++ Models/DetailsEncuestasCiudades.cs
@@ -11,9 +11,10 @@ namespace Esthiber_Valentin_P2_AP1.Models
         public int EncuestaId { get; set; }
 
         [Required(ErrorMessage = "La ciudad es requerida.")]
-        public int CuidadId { get; set; }
+        public int CiudadId { get; set; }
 
         [Required(ErrorMessage = "El monto es requerido.")]
+        [Range(0.01, 500000, ErrorMessage = "El monto debe ser mayor a 0 y menor a 500,000")]
         public double Monto { get; set; } = 0;
 
         [ForeignKey("EncuestaId")]
