e1785fbcfa6defb06c585c681cb111428a2bc781
Esthiber <esthiber1@gmail.com>
2025-03-10 20:11:04 -0400

    Migracion de correccion

---
+++ Migrations/20250311000024_Correccion Details.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Esthiber_Valentin_P2_AP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorreccionDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Monto",
+                table: "DetailsEncuestas",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Monto",
+                table: "DetailsEncuestas");
+        }
+    }
+}
