fec4473bfa25746639eeafc9ad39f02cd59f081d
Esthiber <esthiber1@gmail.com>
2025-03-10 23:41:42 -0400

    Migracion de correccion

--- Migrations/20250311010932_Correccion .Designer.cs
+++ Migrations/20250311010932_Correccion .Designer.cs
@@ -4,6 +4,7 @@ using Esthiber_Valentin_P2_AP1.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Esthiber_Valentin_P2_AP1.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250311010932_Correccion ")]
+    partial class Correccion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
